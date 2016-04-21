954dee87cc22efb92bb06d7f8831878a2c3110a5
thamoui <hamoui.antoine@gmail.com>
2016-04-21 10:35:46 +0200

    test1

--- hello-world.js
+++ hello-world.js
@@ -7,9 +7,8 @@
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  name = name || "World";
+  return "Hello, "+ name + "!";
 };
 
 module.exports = HelloWorld;
